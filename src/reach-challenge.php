41612179b2da1d8b3dfcc1f3ca9e6e35ec3d5c69
Lucas Rocha <lucas.sisau@gmail.com>
2017-10-28 13:35:11 -0300

    Bucket listing with 'size-format' and 'order-by-group' options

--- src/reach-challenge.php
+++ src/reach-challenge.php
@@ -6,34 +6,32 @@ require_once 'vendor/autoload.php';
 class ReachChallenge {
 	protected $version;
 	protected $options;
-	protected $s3client;
-
+	
 	function __construct() {
 		$iniConfig = parse_ini_file('config.ini');
 		$this->version = $iniConfig['version'];
 		$this->options = [
 			'filter' => NULL,
-			'size' => 'bytes',
-			'organize-by-storage-type' => FALSE,
-			'group-by-regions' => FALSE,
+			'group-by-region' => NULL,
+			'size-format' => 'KB',
 		];
-		$this->s3client = new Aws\S3\S3Client([
-			'version' => 'latest',
-			'region'  => 'us-east-1'
-		]);
 	}
 
 	public function main() {
-		global $argc, $argv;
-		if ($argc > 1) {
+		try {
+			global $argv;
 			$this->setParams($argv);
-		} else {
-			$this->showManual();
+			$this->listBuckets();
+			$this->finish();
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		} finally {
+			$this->finish();
 		}
-		$this->finish();
 	}
 
 	protected function setParams($arguments) {
+		array_shift($arguments); //removes filename
 		if (in_array('--help', $arguments)) {
 			$this->showManual();
 		} else if (in_array('--version', $arguments)) {
@@ -42,27 +40,80 @@ class ReachChallenge {
 		foreach ($arguments as $arg) {
 			$explodedArg = explode('=', $arg);
 			$argName = str_replace('--', '', array_shift($explodedArg));
-			switch ($argName) {
-				case 'size':
-					$this->options['size'] = array_pop($explodedArg);
-					break;
-				case 'organize-by-storage-type':
-					$this->options['organize-by-storage-type']  = TRUE;
-					break;
-				case 'group-by-regions':
-					$this->options['group-by-regions'] = TRUE;
-					break;
-				case 'filter':
-					$this->options['filter'] = array_pop($explodedArg);
-					break;
-				default:
-					break;
+			$argValue = array_pop($explodedArg);
+			if (!array_key_exists($argName, $this->options)) {
+				throw new Exception("Unrecognized argument: '$argName'", 1);
+			}
+			if ($argName == 'group-by-region') {
+				$argValue = TRUE;
+			}
+			$this->options[$argName] = $argValue;
+		}
+	}
+
+	protected function listBuckets() {
+		$regionalData = [];
+		$tbl = new Console_Table();
+		$tbl->setHeaders([
+			'Name',
+			'Number of Objects',
+			'Total Size',
+			'Creation Date',
+			'Last Modified',
+		]);
+		$s3Client = (new \Aws\Sdk)->createMultiRegionS3([ 'version' => 'latest' ]);
+		$result = $s3Client->listBuckets();
+		foreach ($result['Buckets'] as $bucket) {
+			$numberOfObjects = 0;
+			$bucketSize = 0;
+			$lastModified = new DateTime('1900-01-01');
+		    $objects = $s3Client->listObjects([ 'Bucket' => $bucket['Name'] ]);
+		    $bucketRegion = $objects['@metadata']['headers']['x-amz-bucket-region'];
+			if ($this->options['group-by-region'] && !isset($regionalData[$bucketRegion])) {
+				$regionalData[$bucketRegion] = [
+					'buckets' => [],
+					'numberOfObjects' => 0,
+					'bucketSize' => 0,
+					'creationDate' => $bucket['CreationDate'],
+					'lastModified' => new DateTime('1900-01-01'),
+				];
+			}
+			foreach ($objects['Contents'] as $file) {
+				if ($file['LastModified'] > $lastModified) {
+					$lastModified = $file['LastModified'];
+				}
+				$numberOfObjects++;
+				$bucketSize += $file['Size'];
+			}
+			if ($this->options['group-by-region']) {
+				if ($lastModified > $regionalData[$bucketRegion]['lastModified']) {
+					$regionalData[$bucketRegion]['lastModified'] = $lastModified;
+				}
+				$regionalData[$bucketRegion]['numberOfObjects'] += $numberOfObjects;
+				$regionalData[$bucketRegion]['bucketSize'] += $bucketSize;
+				$regionalData[$bucketRegion]['buckets'][] = $bucket['Name'];
+			} else {
+				$tbl->addRow([
+			    	$bucket['Name'],
+			    	$numberOfObjects,
+			    	$this->printSize($bucketSize, $this->options['size-format']),
+			    	$bucket['CreationDate']->format('r'),
+			    	$lastModified->format('r'),
+		    	]);
 			}
 		}
-		echo 'Args: ';
-		print_r($arguments);
-		echo 'Options: ';
-		print_r($this->options);
+		if ($this->options['group-by-region']) {
+			foreach ($regionalData as $name => $data) {
+				$tbl->addRow([
+			    	$name . ' (' . implode(', ', $data['buckets']) . ')',
+			    	$data['numberOfObjects'],
+			    	$this->printSize($data['bucketSize'], $this->options['size-format']),
+			    	$data['creationDate']->format('r'),
+			    	$data['lastModified']->format('r'),
+		    	]);
+			}
+		}
+		echo $tbl->getTable();
 	}
 
 	protected function showManual() {
@@ -77,6 +128,28 @@ class ReachChallenge {
 
 	protected function finish() {
 		die("\n");
+	}
+
+	protected function printSize($kbytes, $format = 'bytes') {
+		$bytes = $kbytes * 1024;
+		switch (strtoupper($format))
+		{
+			case 'GB':
+				$result = number_format($bytes / 1073741824, 2) . ' GB';
+				break;
+			case 'MB':
+				$result = number_format($bytes / 1048576, 2) . ' MB';
+				break;
+			case 'KB':
+				$result = number_format($bytes / 1024, 0) . ' KB';
+				break;
+			case 'BYTES':
+				$result = $bytes . ' bytes';
+				break;
+			default:
+				throw new Exception("Unrecognized size format: $format", 1);
+ 		}
+ 		return $result;
 	}
 
 }
